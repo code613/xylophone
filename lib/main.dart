31ba202d0aaed45bdc9c2f7e133435782f3f8ffc
Ben Marcus <32033575+code613@users.noreply.github.com>
2020-10-15 15:48:45 -0400

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+Expanded flat(num) {
+  return Expanded(
+    child: FlatButton(
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note$num.wav');
+      },
+      child: Text('click here'),
+      color: Colors.blue[num * 100],
+    ),
+  );
+}
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +22,18 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              flat(1),
+              flat(2),
+              flat(3),
+              flat(4),
+              flat(5),
+              flat(6),
+              flat(7),
+            ],
+          ),
         ),
       ),
     );
